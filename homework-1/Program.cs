d2d341881355f6128bcb5f49e7d668a7cb542245
Руфина <roofi.g@yandex.ru>
2022-10-10 17:56:23 +0600

    added the first task

--- homework-1/Program.cs
+++ homework-1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int Number1 = -9;
+int Number2 = -3;
+
+if(Number1 > Number2) {
+    Console.Write(Number1);
+} else {
+    Console.Write(Number2);
+}
