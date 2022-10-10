345fad79bfeaf32001d8bf7ae3207de3b4294425
Руфина <roofi.g@yandex.ru>
2022-10-10 19:49:31 +0600

    added task 4

--- homework-1/Program.cs
+++ homework-1/Program.cs
@@ -4,11 +4,29 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-int Number1 = 5;
-int Number2 = 7;
+// int Number1 = 5;
+// int Number2 = 7;
 
-if(Number1 > Number2) {
-    Console.Write($"Большее число: {Number1}, меньшее число : {Number2}");
-} else {
-    Console.Write($"Большее число: {Number2}, меньшее число : {Number1}");
-}
+// if (Number1 > Number2) {
+//     Console.Write($"Большее число: {Number1}, меньшее число : {Number2}");
+// } else {
+//     Console.Write($"Большее число: {Number2}, меньшее число : {Number1}");
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int number1 = 22;
+int number2 = 3;
+int number3 = 9;
+
+int max = 0;
+
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+Console.Write($"Максимальное число: {max}");
