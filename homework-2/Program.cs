cc9d6a2d1418e710742e871f7f766f13166a00ac
Руфина <roofi.g@yandex.ru>
2022-10-18 11:31:51 +0500

    added task 10

---
+++ homework-2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = 918;
+
+int getTheSecondDigit(int number) 
+{
+    int firstTwoDigits = number / 10;
+    int secondDigit = firstTwoDigits % 10;
+    return secondDigit;
+}
+
+Console.WriteLine(getTheSecondDigit(number));
