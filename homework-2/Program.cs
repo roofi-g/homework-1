a708b99fe2fb9aad782cc38a47c0009c62e69f86
Руфина <roofi.g@yandex.ru>
2022-10-18 14:22:12 +0500

    minor fixes

--- homework-2/Program.cs
+++ homework-2/Program.cs
@@ -4,16 +4,16 @@
 // 782 -> 8
 // 918 -> 1
 
-// int number = 918;
+int number = 918;
 
-// int getTheSecondDigit(int number) 
-// {
-//     int firstTwoDigits = number / 10;
-//     int secondDigit = firstTwoDigits % 10;
-//     return secondDigit;
-// }
+int GetTheSecondDigit(int number)
+{
+    int firstTwoDigits = number / 10;
+    int secondDigit = firstTwoDigits % 10;
+    return secondDigit;
+}
 
-// Console.WriteLine(getTheSecondDigit(number));
+Console.WriteLine(GetTheSecondDigit(number));
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
@@ -21,20 +21,23 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-// int number = 32679;
+int number = 32679;
 
-// int getTheThirdDigit(int number) 
-// {
-//     while (number >= 1000) number /= 10;
-//     int theThirdNumber = number % 10;
-//     return theThirdNumber;
-// }
+int GetTheThirdDigit(int number)
+{
+    while (number >= 1000) number /= 10;
+    int theThirdNumber = number % 10;
+    return theThirdNumber;
+}
 
-// if (number >= 0 && number < 100) {
-//     Console.WriteLine("третьей цифры нет");
-// } else {
-//     Console.WriteLine(getTheThirdDigit(number));
-// }
+if (number >= 0 && number < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(GetTheThirdDigit(number));
+}
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
@@ -42,16 +45,18 @@
 // 7 -> да
 // 1 -> нет
 
-int number = 6;
+int numberOfTheDayOfTheWeek = 6;
 
-void Programm(int num)
+void ChecksTheDayOff(int num)
 {
-    if(num == 6 || num == 7){
+    if (num == 6 || num == 7)
+    {
         Console.WriteLine("да");
     }
-    else {
+    else
+    {
         Console.WriteLine("нет");
     }
 }
 
-Programm(number);
+ChecksTheDayOff(numberOfTheDayOfTheWeek);
