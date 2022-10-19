aa24ed03708be95e03909f806627020997b4cf35
Руфина <roofi.g@yandex.ru>
2022-10-19 15:49:20 +0500

    added task 21

--- homework-3/Program.cs
+++ homework-3/Program.cs
@@ -23,3 +23,23 @@ void HeckingForPalindrome(int num)
 }
 
 HeckingForPalindrome(number);
+
+// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+void GetDistanseBetweenTwoPoints(int firstX, int secondX, int firstY, int secondY, int firstZ, int secondZ) 
+{
+    double result = 0;
+
+    double tempX = Math.Pow(secondX - firstX, 2);
+    double tempY = Math.Pow(secondY - firstY, 2);
+    double tempZ = Math.Pow(secondZ - firstZ, 2);
+
+    result = Math.Sqrt(tempX + tempY + tempZ);
+
+    Console.WriteLine($"Расстояние между двумя точками {Math.Round(result,2)}");
+}
+
+GetDistanseBetweenTwoPoints(7, 1, -5, -1, 0, 9);
