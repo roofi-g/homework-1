ec2847fc931cdfc4b2f5d4035092635032534947
Руфина <roofi.g@yandex.ru>
2022-10-19 16:21:54 +0500

    added task 23

--- homework-3/Program.cs
+++ homework-3/Program.cs
@@ -43,3 +43,21 @@ void GetDistanseBetweenTwoPoints(int firstX, int secondX, int firstY, int second
 }
 
 GetDistanseBetweenTwoPoints(7, 1, -5, -1, 0, 9);
+
+// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void GetNumber(int num) 
+{
+    for (int i = 1; i <= num; i++)
+    {
+        double numberInCube = Math.Pow(i, 3);
+        Console.Write($"{numberInCube}, ");
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+GetNumber(number);
