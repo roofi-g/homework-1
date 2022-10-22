73c0a5582e382e5422686ec9636319517febaa6c
roofi-g <88094491+roofi-g@users.noreply.github.com>
2022-10-22 17:35:27 +0500

    Update Program.cs

--- homework-4/Program.cs
+++ homework-4/Program.cs
@@ -3,30 +3,30 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-// int GetNumber(string message)
-// {
-//     Console.WriteLine(message);
-//     int result = int.Parse(Console.ReadLine());
-//     return result;
-// }
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
 
-// int getDegreeOfNumber(int numberA, int numberB)
-// {
-//     int current = 1;
+int getDegreeOfNumber(int numberA, int numberB)
+{
+    int current = 1;
 
-//     for (int i = 0; i < numberB; i++)
-//     {
-//         current = current * numberA;
-//     }
-//     return current;
-// }
+    for (int i = 0; i < numberB; i++)
+    {
+        current = current * numberA;
+    }
+    return current;
+}
 
-// int numberA = GetNumber("Введите число А: ");
-// int numberB = GetNumber("Введите число B: ");
+int numberA = GetNumber("Введите число А: ");
+int numberB = GetNumber("Введите число B: ");
 
-// int result = getDegreeOfNumber(numberA, numberB);
+int result = getDegreeOfNumber(numberA, numberB);
 
-// Console.WriteLine($"Число {numberA} в степени {numberB} равно {result}");
+Console.WriteLine($"Число {numberA} в степени {numberB} равно {result}");
 
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
@@ -35,30 +35,30 @@
 // 82 -> 10
 // 9012 -> 12
 
-// int GetNumber(string message)
-// {
-//     Console.WriteLine(message);
-//     int result = int.Parse(Console.ReadLine() ?? "");
-//     return result;
-// }
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine() ?? "");
+    return result;
+}
 
-// int getSumOfDigits(int number)
-// {
-//     int sum = 0;
-//     while(number > 0)
-//     {
-//         int lastDigit = number % 10;
-//         sum = sum + lastDigit;
+int getSumOfDigits(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        int lastDigit = number % 10;
+        sum = sum + lastDigit;
 
-//         number = number / 10;
-//     }
-//     return sum;
-// }
+        number = number / 10;
+    }
+    return sum;
+}
 
-// int numb = GetNumber("Введите положительное число: ");
-// int result = getSumOfDigits(numb);
+int numb = GetNumber("Введите положительное число: ");
+int result = getSumOfDigits(numb);
 
-// Console.WriteLine($"Сумма цифр в числе {numb} равен {result}");
+Console.WriteLine($"Сумма цифр в числе {numb} равен {result}");
 
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
@@ -67,8 +67,8 @@
 // 6, 1, 33 -> [6, 1, 33]
 // ввод элеменетов через консоль (через запятую. использовать метод string.Split())
 
-// Console.WriteLine("Введите 8 элементов через запятую: ");
+Console.WriteLine("Введите 8 элементов через запятую: ");
 
-// string[] array = Console.ReadLine().Split(new char[] {','});
+string[] array = Console.ReadLine().Split(new char[] {','});
 
-// Console.WriteLine(string.Join(",", array));
+Console.WriteLine(string.Join(",", array));
